089c43255d1f8fd35b1623b774c8b2fb5bc71a70
jrosbrow <45439168+jrosbrow@users.noreply.github.com>
2020-10-10 00:21:24 -0400

    Add charting

--- index.js
+++ index.js
@@ -23,10 +23,14 @@ async function gatherResponse(response) {
 }
 
 async function handleRequest(request) {
+
+  var asciichart = require ('asciichart');
+
   var url = request.url.split("/");
   var ticker = url.slice(-1)[0];
   var yf_url = "https://query1.finance.yahoo.com/v8/finance/chart/";
-  var ticker_url = yf_url.concat(ticker);
+  var params = "?interval=15m"
+  var ticker_url = yf_url.concat(ticker, params);
 
   const init = {
     headers: {
@@ -38,12 +42,27 @@ async function handleRequest(request) {
 
   const price = results.chart.result[0].meta.regularMarketPrice;
   const timestamp = results.chart.result[0].meta.regularMarketTime;
+  const prev_close = results.chart.result[0].meta.previousClose;
+
+  const chart_data_high = results.chart.result[0].indicators.quote[0].high;
+
+  if (prev_close > price) {
+    color = asciichart.red
+  } else {
+    color = asciichart.green
+  };
+
+  var config = {
+    colors: [color]
+  };
+
+  const chart = asciichart.plot ([chart_data_high], config);
 
   var human_time = new Date();
   human_time.setTime(timestamp*1000);
   human_time_string = human_time.toUTCString();
 
-  const output = ticker + " " + price + " " + human_time_string
+  const output = ticker + " " + price + " " + human_time_string + "\n\n" + chart
   return new Response(output, {
     headers: { 'content-type': 'text/plain'},
   })
